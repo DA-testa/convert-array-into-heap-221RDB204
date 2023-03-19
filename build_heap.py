dc88419f1b3417b3a707ae22d000104fffe24a50
Frīdis Ludiņš <124689784+221RDB204@users.noreply.github.com>
2023-03-19 14:30:01 +0000

    test

--- build_heap.py
+++ build_heap.py
@@ -3,10 +3,19 @@
 
 def build_heap(data):
     swaps = []
+    n = len(data)
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
-
+    for i in range(n//2, -1, -1):
+        while True:
+            if 2*i+1 <= n-1 and data[i]>data[2*i+1]:
+                swaps.append((i, 2*i+1))
+                data[i], data[2*i+1] = data[2*i+1], data[i]
+            if 2*i+2 <= n-1 and data[i]>data[2*i+2]:
+                swaps.append((i, 2*i+2))
+                data[i], data[2*i+2] = data[2*i+2], data[i]
+            else:
+                break
     return swaps
 
 
@@ -15,12 +24,20 @@ def main():
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
+    mode = input()
+    if mode[0] =="I":
 
     # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
+        n = int(input())
+        data = list(map(int, input().split()))
+    elif mode[0] == "F":
+        file_name = input()
+        file_name = 'tests/' + file_name
+        with open(file_name, 'r') as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
+    else:
+        return
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
